b297282f86a3ba8e00fc34848a2a9a60efa9ecf0
SivanandhanM <sivanandhanmanimuthu@gmail.com>
2026-07-06 15:49:15 +0530

    Fix GitHub Pages routing using HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import MainLayout from './layouts/MainLayout';
+
 import Home from './pages/Home/Home';
 import Purchase from './pages/Purchase/Purchase';
 import Sales from './pages/Sales/Sales';
@@ -13,10 +15,11 @@ import GST from './pages/GST/GST';
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+
           <Route path="purchase" element={<Purchase />} />
           <Route path="sales" element={<Sales />} />
           <Route path="billing" element={<Billing />} />
@@ -26,10 +29,11 @@ function App() {
           <Route path="banking" element={<Banking />} />
           <Route path="gate-management" element={<GateManagement />} />
           <Route path="gst" element={<GST />} />
+
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
