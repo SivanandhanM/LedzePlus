5a8a8c947f3d023c1525987b57babcd3070de3c2
SivanandhanM <sivanandhanmanimuthu@gmail.com>
2026-07-18 14:15:43 +0530

    fix: remove unused useState

--- src/components/GstLearning/GstModal.tsx
+++ src/components/GstLearning/GstModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { motion, LayoutGroup } from 'framer-motion';
 import type { GstModuleData } from '../../data/gstLearningData';
